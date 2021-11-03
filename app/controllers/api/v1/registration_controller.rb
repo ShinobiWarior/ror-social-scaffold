cfb914feaa1e903c2a66db37ee9c9c13462c130c
Aleksandar <a.ivezic@yahoo.com>
2021-11-03 14:41:27 +0100

    Create RegistrationController

---
+++ app/controllers/api/v1/registration_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RegistrationController < ApiController
+  skip_before_action :authenticate_user!
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: {token: JsonWebToken.encode(sub: @user.id)}
+    else
+      render json: { errors: "Registration failed!"}
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
