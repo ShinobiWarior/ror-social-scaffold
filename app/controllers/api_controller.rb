24c1e928fb7c1b75f594b47a0040a5e596a5ab50
Aleksandar <a.ivezic@yahoo.com>
2021-11-03 14:40:47 +0100

    Update ApiController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,7 @@
 class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_default_format
-
+  before_action :authenticate_user!
   private
 
   def set_default_format
